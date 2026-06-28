fe13e9642401febce8d7dc60c7fcb965f8a5e880
upanshuv <upanshu.v@gmail.com>
2026-06-28 19:05:52 -0400

    changing app theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'otterlist-v1';
+const CACHE = 'otterquests-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
